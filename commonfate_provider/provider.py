ed4a1d50df00b5e246bb9b4822f21933ed64eb30
Manish Chaulagain <manish23chaulagain@gmail.com>
2023-01-27 17:55:25 +1100

    export usage string if available (#9)

--- commonfate_provider/provider.py
+++ commonfate_provider/provider.py
@@ -110,7 +110,7 @@ class Provider(ABC):
         all_vars = [(k, v) for (k, v) in vars(cls).items() if not k.startswith("__")]
         for k, v in all_vars:
             if type(v) == String:
-                config_vars[k] = {"type": "string"}
+                config_vars[k] = {"type": "string", "usage": v.usage}
         return config_vars
 
 
