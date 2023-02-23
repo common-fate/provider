8d438731cbd47683dc4000e63da25d1a9a5d691b
chrnorm <17420369+chrnorm@users.noreply.github.com>
2023-02-23 22:48:10 +0000

    fix schema method

--- commonfate_provider/cli/main.py
+++ commonfate_provider/cli/main.py
@@ -1,13 +1,34 @@
+import importlib
 import json
+import pkgutil
 from commonfate_provider import resources
 from commonfate_provider.loader import load_provider
 import click
 
 
+def import_submodules(package, recursive=True):
+    """Import all submodules of a module, recursively, including subpackages
+
+    :param package: package (name or actual module)
+    :type package: str | module
+    :rtype: dict[str, types.ModuleType]
+    """
+    if isinstance(package, str):
+        package = importlib.import_module(package)
+    results = {}
+    for loader, name, is_pkg in pkgutil.walk_packages(package.__path__):
+        full_name = package.__name__ + "." + name
+        results[full_name] = importlib.import_module(full_name)
+        if recursive and is_pkg:
+            results.update(import_submodules(full_name))
+    return results
+
+
 @click.command()
 @click.option("--dir", default=".", help="Directory to the load the provider from")
 def schema(dir):
-    Provider, Args = load_provider(dir)
+    import_submodules(dir)
+    Provider, Args = load_provider()
     schema = {}
 
     schema["config"] = Provider.export_schema()
