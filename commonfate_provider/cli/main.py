d599f1b7b1f446d1fccf9d9638707ad933968692
Chris Norman <17420369+chrnorm@users.noreply.github.com>
2023-02-23 22:59:55 +0000

    fix schema method (#30)
    * fix schema method
    
    * revert back to using toml to load class for now

--- commonfate_provider/cli/main.py
+++ commonfate_provider/cli/main.py
@@ -1,7 +1,27 @@
+import importlib
 import json
+import pkgutil
 from commonfate_provider import resources
 from commonfate_provider.loader import load_provider
 import click
+
+
+def import_submodules(package, rel_name=None, recursive=True):
+    """Import all submodules of a module, recursively, including subpackages
+
+    :param package: package (name or actual module)
+    :type package: str | module
+    :rtype: dict[str, types.ModuleType]
+    """
+    if isinstance(package, str):
+        package = importlib.import_module(package, rel_name)
+    results = {}
+    for loader, name, is_pkg in pkgutil.walk_packages(package.__path__):
+        full_name = package.__name__ + "." + name
+        results[full_name] = importlib.import_module(full_name)
+        if recursive and is_pkg:
+            results.update(import_submodules(full_name))
+    return results
 
 
 @click.command()
--- commonfate_provider/loader.py
+++ commonfate_provider/loader.py
@@ -1,9 +1,60 @@
+import importlib
+import os
 import typing
 from commonfate_provider.provider import Provider
 from commonfate_provider.args import Args
+import toml
 
 
-def load_provider() -> typing.Tuple[typing.Type[Provider], typing.Type[Args]]:
+def load_provider(
+    cwd: str = "",
+) -> typing.Tuple[typing.Type[Provider], typing.Type[Args]]:
+    """
+    Loads the Common Fate Provider by reading the provider.toml file.
+    This method dynamically instantiates the provider class.
+    """
+    l = os.path.join(cwd, "provider.toml")
+    config = toml.load(l)
+
+    language = config["language"]
+
+    if language != "python3.9":
+        # this wrapping code is written in Python, so we can only handle
+        # Providers written in Python.
+        raise Exception(f"invalid language: {language}")
+
+    provider_class = load_class(cwd, config["python"]["class"])
+    arg_class = load_class(cwd, config["python"]["arg_schema"])
+    return (provider_class, arg_class)
+
+
+def load_class(cwd: str, path: str):
+    """
+    Dynamically loads a class. The path argument is the path
+    to the class in the format 'module.ClassName'
+    """
+    components = path.split(".")  # [module, ClassName]
+    modulePathWithinProviderFolder = "/".join(components[:-1]) + ".py"
+
+    # Import the module using an absolute path to the python file containing the Provider class
+    spec = importlib.util.spec_from_file_location(
+        "provider", os.path.join(cwd, modulePathWithinProviderFolder)
+    )
+    # Loading the module
+
+    if spec is None or spec.loader is None:
+        raise Exception("expected spec and spec.loader not to be None")
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+
+    class_name = components[-1]  # ClassName
+    my_class = getattr(module, class_name)
+    return my_class
+
+
+def load_provider_from_subclass() -> (
+    typing.Tuple[typing.Type[Provider], typing.Type[Args]]
+):
     """
     Loads the Common Fate Provider.
     This method dynamically instantiates the provider class.
