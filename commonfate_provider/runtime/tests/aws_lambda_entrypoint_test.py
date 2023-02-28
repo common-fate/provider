e6317080f7bd427d50b9206b6af311f2eb1c14c9
chrnorm <17420369+chrnorm@users.noreply.github.com>
2023-02-28 14:43:59 +0000

    fix test

--- commonfate_provider/runtime/tests/aws_lambda_entrypoint_test.py
+++ commonfate_provider/runtime/tests/aws_lambda_entrypoint_test.py
@@ -12,13 +12,14 @@ def fresh_namespace():
     namespace.clear()
 
 
-@pytest.fixture
-def dist_module():
-    old_path = sys.path
-    sys.path.append(path.join(path.dirname(__file__)))
-    yield
-    sys.path = old_path
+# @pytest.fixture
+# def dist_module():
+#     old_path = sys.path
+
+#     yield
+#     sys.path = old_path
 
 
 def test_entrypoint_works(dist_module):
+    sys.path.append(path.join(path.dirname(__file__)))
     from commonfate_provider.runtime import aws_lambda_entrypoint
