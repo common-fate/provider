6491acaf48398efcb0a8a0733bccb949eab53f2e
Shwetha Umashanker <shwthshanker@gmail.com>
2023-05-11 12:35:37 -0400

    Fix AttributeError: 'String' object has no attribute '_value' to give better error message

--- provider/tests/provider_test.py
+++ provider/tests/provider_test.py
@@ -22,3 +22,11 @@ def test_export_schema_works(example_provider):
     got = ExampleProvider.export_config_schema()
     want = {"value": {"type": "string", "description": None, "secret": False}}
     assert got == want
+
+
+def test_config_value_missing(example_provider):
+    ExampleProvider = example_provider
+    p = ExampleProvider()
+    with pytest.raises(Exception) as exc_info:
+        p.value.get()
+    assert str(exc_info.value) == "A required config value is not set"
